2c92c86658d3b3726281281ccf472314ede9cdec
sujeetamberkar <sujeet.amberkar2002@outlook.com>
2023-09-09 18:15:23 +0530

    Salted Hshing

---
+++ 9_Salted_Hashing/Mod_Sha512.py
@@ -0,0 +1,64 @@
+from hashlib import sha512
+import random
+from qiskit import QuantumCircuit, Aer, execute
+
+def prepare_qubit(bit, basis):
+    qc = QuantumCircuit(1, 1)
+    if bit == 1:
+        qc.x(0)
+    if basis == 'x':
+        qc.h(0)
+    return qc
+
+def measure_qubit(qc, basis):
+    if basis == 'x':
+        qc.h(0)
+    qc.measure(0, 0)
+    backend = Aer.get_backend('qasm_simulator')
+    job = execute(qc, backend, shots=1, memory=True)
+    result = job.result()
+    return int(result.get_memory()[0])
+
+def generate_binary_key():
+    num_bits = 256
+
+    alice_bits = [random.choice([0, 1]) for _ in range(num_bits)]
+    alice_bases = [random.choice(['+', 'x']) for _ in range(num_bits)]
+    alice_qubits = [prepare_qubit(alice_bits[i], alice_bases[i]) for i in range(num_bits)]
+
+    bob_bases = [random.choice(['+', 'x']) for _ in range(num_bits)]
+    bob_bits = [measure_qubit(alice_qubits[i], bob_bases[i]) for i in range(num_bits)]
+
+    shared_key = [alice_bits[i] for i in range(num_bits) if alice_bases[i] == bob_bases[i]]
+
+    while len(shared_key) < 128:
+        additional_key = generate_binary_key()
+        shared_key.extend(additional_key)
+
+    return shared_key[:128]
+
+def binary_to_hex(binary_key):
+    binary_str = ''.join(map(str, binary_key))
+    return hex(int(binary_str, 2))[2:].upper()
+
+def generate_key():
+    binary_key = generate_binary_key()
+    return binary_to_hex(binary_key)
+
+def hashing(plainText,salt=None):
+    if salt is None:
+        salt=generate_key()
+    hash_value=sha512((str(salt)+plainText).encode()).hexdigest()
+    return salt, hash_value
+
+def check_hashing(plainText,salt):
+    hash_value=sha512((str(salt)+plainText).encode()).hexdigest()
+    return hash_value
+
+if __name__ == '__main__':
+    salt_value1,hash_value1=hashing("Sujet")
+    print("Salt Value ",salt_value1)
+    print("Hash Value ", hash_value1,'\n')
+    salt_value2,hash_value2=hashing("Sujet")
+    print("Salt Value ",salt_value2)
+    print("Hash Value ", hash_value2,'\n')
---
+++ 9_Salted_Hashing/Readme.md
@@ -0,0 +1,62 @@
+# Quantum Salted Hashing
+This module offers functionalities to utilize quantum properties for generating salt values which are then used for creating SHA-512 hashes. The core principle behind the code is Quantum Key Distribution (QKD), a method by which cryptographic keys can be shared securely using quantum mechanics. In this implementation, this property is leveraged to produce unique salts to improve security.
+
+## Requirements:
+Python (3.x recommended)
+Qiskit library
+
+## How to Install Qiskit:
+To install the required Qiskit library, you can use pip:
+
+```
+pip install qiskit
+
+```
+
+## Features:
+
+1) prepare_qubit(bit, basis)
+    ```
+    Prepares a qubit based on the given bit and basis.
+    bit: 0 or 1, represents the bit value.
+    basis: '+' or 'x', represents the measurement basis.
+    
+```
+    
+2) measure_qubit(qc, basis)
+```
+    Measures the qubit in the given basis.
+    qc: A QuantumCircuit containing the qubit.
+    basis: '+' or 'x', the measurement basis.
+```
+3) generate_binary_key()
+```
+    Generates a binary key using Quantum Key Distribution.
+```
+
+4) binary_to_hex(binary_key)
+```
+    Converts a binary key to its hex representation.
+    binary_key: The binary key as a list of 0's and 1's.
+```
+5) generate_key()
+```
+    Generates a salt key using Quantum Key Distribution and converts it to hexadecimal.
+```
+6) hashing(plainText, salt=None)
+```
+    Generates a SHA-512 hash of the provided plainText, salted with the provided salt. If no salt is provided, a quantum-generated salt is used.
+    plainText: The text to be hashed.
+    salt: The salt to use. If not provided, a new quantum-generated salt is created.
+```
+7) check_hashing(plainText, salt)
+```
+    Generates a SHA-512 hash for the provided plainText using the given salt.
+    plainText: The text to be hashed.
+    salt: The salt to use for hashing.
+
+```
+```
+### Author
+Sujeet Amberkar
+
